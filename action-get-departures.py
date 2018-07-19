55c61b8f94e9f29140a6bae590ce5ac8b5b06fad
Laurenz Thiel <l.thiel@hotmail.de>
2018-07-19 02:20:58 +0200

    added config.ini in order to set a default station (untested)

--- action-get-departures.py
+++ action-get-departures.py
@@ -16,6 +16,7 @@ import math
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
+
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
@@ -30,9 +31,31 @@ def read_configuration_file(configuration_file):
     except (IOError, ConfigParser.Error) as e:
         return dict()
 
+
 def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
     action_wrapper(hermes, intentMessage, conf)
+
+
+def _generate_result_sentence(departures, station_name):
+    result_sentence = ''
+
+    if len(departures) is 0:
+        result_sentence += "Es wurden keine Abfahrten für {} gefunden.".format(station_name)
+    else:
+        result_sentence += "Die nächsten Abfahrten ab {} sind: ".format(station_name)
+        for departure in departures:
+            time_delta = math.floor(((departure.time - datetime.now()).total_seconds() / 60))
+            if time_delta > 0:
+                temp_sentence = "Linie {} in {} Minuten in Richtung {}. ".format(departure.route.encode("utf8"),
+                                                                                 str(time_delta).split('.')[0],
+                                                                                 departure.destination.encode("utf8"))
+            else:
+                temp_sentence = "Linie {} fährt jetzt in Richtung {}. ".format(departure.route.encode("utf8"),
+                                                                               departure.destination.encode("utf8"))
+            result_sentence += temp_sentence
+    return result_sentence
+
 
 def _search_for_station_id(station_name):
     result_id = -1
@@ -49,6 +72,7 @@ def _search_for_station_id(station_name):
             result_id = result_list[0].stop_id
     return result_id
 
+
 def action_wrapper(hermes, intentMessage, conf):
     """ Write the body of the function that will be executed once the intent is recognized.
     In your scope, you have the following objects :
@@ -57,38 +81,28 @@ def action_wrapper(hermes, intentMessage, conf):
     - conf : a dictionary that holds the skills parameters you defined
     Refer to the documentation for further details.
     """
-    station_name = "büchig"
+
+    default_station_name = conf['secret']['default_station']
+    station_name = "Karlsruhe Hbf Vorplatz"
+
+    if default_station_name:
+        station_name = default_station_name
+
     try:
         station_name = str(intentMessage.slots.station_name.first().value)
     except:
         pass
 
     next_departures = []
-    station_id = 'de:8212:3013'
+    search_result = _search_for_station_id(station_name)
 
-    if station_name != "büchig":
-        search_result = _search_for_station_id(station_name)
-        if search_result is not -1:
-            station_name, station_id = search_result
-            next_departures = kvv.get_departures(station_id, 4)
-
-    result_sentence = ""
-
-    if len(next_departures) is 0:
-        result_sentence += "Es wurden keine Abfahrten für {} gefunden.".format(station_name)
-    else:
-        result_sentence += "Die nächsten Abfahrten ab {} sind: ".format(station_name)
-        for dep in next_departures:
-            time_delta = math.floor(((dep.time - datetime.now()).total_seconds() / 60))
-            temp_sentence = ""
-            if time_delta > 0:
-                temp_sentence = "Linie {} in {} Minuten in Richtung {}. ".format(dep.route.encode("utf8"), str(time_delta).split('.')[0], dep.destination.encode("utf8"))
-            else:
-                temp_sentence = "Linie {} fährt jetzt in Richtung {}. ".format(dep.route.encode("utf8"), dep.destination.encode("utf8"))
-            result_sentence += temp_sentence
+    if search_result is not -1:
+        station_name, station_id = search_result
+        next_departures = kvv.get_departures(station_id, 4)
 
     current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+    hermes.publish_end_session(current_session_id, _generate_result_sentence(next_departures, station_name))
+
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
